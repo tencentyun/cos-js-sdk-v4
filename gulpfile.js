6b978b3fdad200e390df7e91b28dae85e6b5e71b
carsonxu <459452372@qq.com>
2017-05-25 08:02:54 +0800

    move gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/* global require */
+var gulp = require('gulp'), gulpLoadPlugins = require('gulp-load-plugins'), plugins = gulpLoadPlugins();
+var uglify = require('gulp-uglify');
+
+gulp.task('default', ['dev']);
+
+gulp.task('dev', ['build'], function () {
+    gulp.watch('./src/*.js', ['build']);
+});
+
+gulp.task('build', function () {
+    gulp.src(['./src/*.js'])
+        .pipe(uglify({
+            preserveComments:'license'
+        }))
+        .pipe(plugins.concat('cos-js-sdk-v4.js'))
+        .pipe(gulp.dest('./dist').on('finish', function () {
+            console.log('concat done...');
+        }));
+});
